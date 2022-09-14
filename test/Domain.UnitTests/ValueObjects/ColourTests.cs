f8365ea571f95f59a41e3eb1af27f8362ca11142
Surendra <INFO.srmp.IN@gmail.com>
2022-09-14 22:32:56 +0530

    Domain- NUnit test done

---
+++ test/Domain.UnitTests/ValueObjects/ColourTests.cs
@@ -0,0 +1,24 @@
+﻿using Domain.ValueObjects;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UnitTests.ValueObjects
+{
+    public class ColourTests
+    {
+        [Test]
+        public void ShouldReturnCorrectColourCode()
+        {
+            var code = "#FFFFFF";
+
+            var colour = Colour.From(code);
+
+            colour.Code.Should().Be(code);
+        }
+
+    }
+}

aaca6e48a8192ec51a98f484ad51e1ee6f9e59eb
surendrasingh599 <surendrasingh599@gmail.com>
2022-09-15 21:53:33 +0530

    Domain-Unit Test For UnsupportedColour

---
+++ test/Domain.UnitTests/Exceptions/UnsupportedColourExceptionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Exceptions;
+using FluentAssertions.Equivalency;
+
+namespace Domain.UnitTests.Exceptions
+{
+    public class UnsupportedColourExceptionTest
+    {
+        [Test]
+        public void ShouldReturnUnsupportedColourException()
+        {
+            var UnsupportedColour = "#FFC300";
+            var ex = Assert.Throws<UnsupportedColourException>(() => throw new UnsupportedColourException(UnsupportedColour));
+            Assert.That(ex.Message, Is.EqualTo($"Colour \"{UnsupportedColour}\" is unsupported."));
+
+        }
+
+    }
+}
+
+

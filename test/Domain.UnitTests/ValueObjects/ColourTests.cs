f8966456c59ba062dd432bbfe8a2dcf349fe56ff
surendrasingh599 <surendrasingh599@gmail.com>
2022-09-15 21:33:47 +0530

    Domain-Unit Test Added

--- test/Domain.UnitTests/ValueObjects/ColourTests.cs
+++ test/Domain.UnitTests/ValueObjects/ColourTests.cs
@@ -8,17 +8,55 @@ using System.Threading.Tasks;
 
 namespace Domain.UnitTests.ValueObjects
 {
+     
+
     public class ColourTests
     {
         [Test]
-        public void ShouldReturnCorrectColourCode()
+        [TestCase("#FF5733")]
+        [TestCase("#FFC300")]
+        [TestCase("#FFFF66")]
+        [TestCase("#FFFFFF")]
+
+        public void ShouldReturnCorrectColourCode(string ColorCode)
         {
-            var code = "#FFFFFF";
+            var code = ColorCode;// "#FFFFFF";
 
             var colour = Colour.From(code);
 
-            colour.Code.Should().Be(code);
+            //colour.Code.Should().Be(code);
+
+            Assert.That(colour.Code, Is.EqualTo(code));
         }
+
+       
+
+        [Test]
+        public void ImplicitColourCodeToString()
+        {
+            var color =(string) Colour.White;
+
+            Assert.That(color, Is.EqualTo("#FFFFFF"));
+
+        }
+        [Test]
+        public void ExceptionColourCodeToString()
+        {
+            var color = (Colour)"#FFFFFF";
+
+            Assert.That(color, Is.EqualTo(Colour.White));
+
+        }
+        [Test]
+        public void ToStringRetrunCode()
+        {
+            var color = Colour.White;
+
+            Assert.That(color.Code, Is.EqualTo(color.ToString()));
+
+        }
+
+       
 
     }
 }
